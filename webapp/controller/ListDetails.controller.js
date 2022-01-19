2b59f03174e08528ea8af20918ed38651106ad32
Huaramo <jrdwarf222222@gmail.com>
2022-01-19 17:32:21 +0100

    ADD: UI-Elemente in Master-, ListDetails und ListSettings Views

--- webapp/controller/ListDetails.controller.js
+++ webapp/controller/ListDetails.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
-], function(Controller, History) {
+	"sap/ui/core/routing/History",
+	"sap/ui/core/Fragment"
+], function(Controller, History, Fragment) {
 	"use strict";
 	return Controller.extend("qstMyEinkauf.controller.ListDetails", {
 		onInit: function() {
@@ -35,6 +36,32 @@ sap.ui.define([
 				// Naviate to master page
 				this.getOwnerComponent().getRouter().navTo("master", {}, true);
 			}
-		}
-	});
+		},
+		
+		onOpenDialog: function () {
+			var oView = this.getView();
+			// create dialog lazily
+			if (!this.byId("openDialog")) {
+			// load asynchronous XML fragment
+			Fragment.load({
+				id: oView.getId(),
+				name: "qstMyEinkauf.view.Dialog",
+				controller: this
+				}).then(function (oDialog) {
+			// connect dialog to the root view 
+			//of this component (models, lifecycle)
+    		oView.addDependent(oDialog);
+    		oDialog.open();
+			});
+			} else {
+    			this.byId("openDialog").open();
+    		}
+    		},
+ 
+    		closeDialog: function () {
+        			this.byId("openDialog").close();
+    			}
+		
+		
+			});
 });
