035f0e233915f801620756f58680ff01f0ea23b0
KÃ¤the <kaethe.weit@yahoo.de>
2022-01-21 11:25:27 +0100

    UPDATE: Code übersichtlicher & korrekt einrücken

--- webapp/controller/ListDetails.controller.js
+++ webapp/controller/ListDetails.controller.js
@@ -15,20 +15,20 @@ sap.ui.define([
 				model: "beispiel"
 			}); //orderID anpassen + "/orders/" muss zu Modell passen! + Modell anpassen + Seite 240 Extras einbauen
 		},
-		
+
 		/*onSelectionChange: function(oEvent) {
 			var sProductId = oEvent.getSource().getBindingContext().getProperty("productId");
 			this.getOwnerComponent().getRouter()
 				.navTo("productDetails",
 					{orderId:this._orderId, productId: sProductId});
 		},*/
-		
-		
+
 		onToSettings: function() {
-			this.getOwnerComponent().getRouter().navTo("listSettings", {orderId:this._orderId}); //orderID anpassen
+			this.getOwnerComponent().getRouter().navTo("listSettings", {
+				orderId: this._orderId
+			}); //orderID anpassen
 		},
-		
-		
+
 		// Für Routing zurück im Browser
 		onNavBack: function() {
 			var sPreviousHash = History.getInstance().getPreviousHash();
@@ -41,33 +41,32 @@ sap.ui.define([
 				this.getOwnerComponent().getRouter().navTo("master", {}, true);
 			}
 		},
-		
-		onOpenDialog: function () {
+
+		onOpenDialog: function() {
 			var oView = this.getView();
 			// create dialog lazily
 			if (!this.byId("openDialog")) {
-			// load asynchronous XML fragment
+				// load asynchronous XML fragment
 				Fragment.load({
 					id: oView.getId(),
 					name: "qstMyEinkauf.view.Dialog",
 					controller: this
-				}).then(function (oDialog) {
-				// connect dialog to the root view 
-				//of this component (models, lifecycle)
-    			oView.addDependent(oDialog);
-    			oDialog.open();
-			});
+				}).then(function(oDialog) {
+					// connect dialog to the root view 
+					//of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
 			} else {
-    			this.byId("openDialog").open();
-    		}
-    		
-    		//oView.byId("").setText("");
-    	},
- 
-    	closeDialog: function () {
-    			this.byId("openDialog").close();
-    	}
-		
-		
-		});
+				this.byId("openDialog").open();
+			}
+
+			//oView.byId("").setText("");
+		},
+
+		closeDialog: function() {
+			this.byId("openDialog").close();
+		}
+
+	});
 });
--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,23 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller", 
+	"sap/ui/core/mvc/Controller",
 	"sap/ui/Device",
 	"sap/m/MessageToast",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/Sorter"
-	], function (Controller, Device, MessageToast, Filter, FilterOperator, Sorter) {
+], function(Controller, Device, MessageToast, Filter, FilterOperator, Sorter) {
 	"use strict";
 
 	return Controller.extend("qstMyEinkauf.controller.Master", {
-		onInit : function () {
+		onInit: function() {
 			this.getOwnerComponent().getRouter().getRoute("master").attachPatternMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched: function() {
 			/*
-			* Navigate to the first item by default only on desktop and tablet (but not phone).
-			* Note that item selection is not handled as it is
-			* out of scope of this sample
-			*/
+			 * Navigate to the first item by default only on desktop and tablet (but not phone).
+			 * Note that item selection is not handled as it is
+			 * out of scope of this sample
+			 */
 			if (!Device.system.phone) {
 				this.getOwnerComponent().getRouter()
 					.navTo("listStart", true); //
@@ -27,73 +27,71 @@ sap.ui.define([
 			MessageToast.show("Enter SelectionChange");
 			var sListId = oEvent.getSource().getSelectedItem().getBindingContext("beispiel").getProperty("orderId"); // "orderId" aus dem Modell!! Property und nicht der Pfad! Zufällig das Gleiche
 			this.getOwnerComponent().getRouter()
-				.navTo("listDetails",
-					{orderId:sListId}, // an Modell anpassen!
+				.navTo("listDetails", {
+						orderId: sListId
+					}, // an Modell anpassen!
 					!Device.system.phone);
 		},
-		
-		onSelectName: function(oEvent){
-			var oItem = oEvent.getParameter("selectedItem");
-        	var sPath = oItem.getBindingContext("beispiel").getPath();
-        	var oList = this.getView().byId("el");
-        	var sOwner = this.getView().getModel("beispiel").getProperty(sPath + "/owner");
-        	oList.bindElement(sPath);
-        	
-        	var oFilter = new Filter({
-                     path: "owner",
-                    operator: FilterOperator.EQ,
-                    value1: sOwner
-                     });
-        
-        	oList.getBinding("items").filter(oFilter);
-		},
-		onSelectSort: function(oEvent){
-            MessageToast.show("Sorted!");
-            var oItem = oEvent.getParameter("selectedItem");
-        	var sText = oItem.getText();
-        	var oList = this.getView().byId("el");
-        	//var sOwner = this.getView().getModel("beispiel").getProperty(sPath + "/owner");
-        	if(sText === "Ersteller"){
-        		var oSorter = new Sorter("owner");
-        		oList.getBinding("items").sort(oSorter);
-        	}else if(sText === "Zeit"){
-        		var oSorter = new Sorter("changeDate");
-        		oSorter.fnComparator = this.comparator;
-        		oList.getBinding("items").sort(oSorter);
-        	}
 
-        },
-        
-        onReset: function(oEvent){
-            MessageToast.show("Reset!");
-        	var oList = this.getView().byId("el");
-        	oList.getBinding("items").filter(null);
-        	oList.getBinding("items").sort(null);
-        
-        },
-        comparator: function(a, b){
-        	 
-                
-        	
-            var aDate = new Date(a);
-            var bDate = new Date(b);
-                    
-            if (bDate == null) {
-                return -1;
-            }
-            if (aDate == null) {
-                return 1;
-            }
-            if (aDate < bDate) {
-                return -1;
-            }
-            if (aDate > bDate) {
-                return 1;
-            }
-            return 0;
-            
-        }
-        
-		
+		onSelectName: function(oEvent) {
+			var oItem = oEvent.getParameter("selectedItem");
+			var sPath = oItem.getBindingContext("beispiel").getPath();
+			var oList = this.getView().byId("el");
+			var sOwner = this.getView().getModel("beispiel").getProperty(sPath + "/owner");
+			oList.bindElement(sPath);
+
+			var oFilter = new Filter({
+				path: "owner",
+				operator: FilterOperator.EQ,
+				value1: sOwner
+			});
+
+			oList.getBinding("items").filter(oFilter);
+		},
+		onSelectSort: function(oEvent) {
+			MessageToast.show("Sorted!");
+			var oItem = oEvent.getParameter("selectedItem");
+			var sText = oItem.getText();
+			var oList = this.getView().byId("el");
+			//var sOwner = this.getView().getModel("beispiel").getProperty(sPath + "/owner");
+			if (sText === "Ersteller") {
+				var oSorter = new Sorter("owner");
+				oList.getBinding("items").sort(oSorter);
+			} else if (sText === "Zeit") {
+				var oSorter = new Sorter("changeDate");
+				oSorter.fnComparator = this.comparator;
+				oList.getBinding("items").sort(oSorter);
+			}
+
+		},
+
+		onReset: function(oEvent) {
+			MessageToast.show("Reset!");
+			var oList = this.getView().byId("el");
+			oList.getBinding("items").filter(null);
+			oList.getBinding("items").sort(null);
+
+		},
+		comparator: function(a, b) {
+
+			var aDate = new Date(a);
+			var bDate = new Date(b);
+
+			if (bDate == null) {
+				return -1;
+			}
+			if (aDate == null) {
+				return 1;
+			}
+			if (aDate < bDate) {
+				return -1;
+			}
+			if (aDate > bDate) {
+				return 1;
+			}
+			return 0;
+
+		}
+
 	});
 });
